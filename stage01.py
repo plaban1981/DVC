f3d98336b4838f5ace34d8e86a8d11fe63b4e307
Plaban1981 <nayakpplaban@gmail.com>
2021-09-25 09:38:01 +0530

    first draft

---
+++ stage01.py
@@ -0,0 +1,2 @@
+with open("artifacts01.txt","w+") as f:
+    f.write("text in stage01.py")
---
+++ stage02.py
@@ -0,0 +1,4 @@
+with open('artifacts01.txt',"r") as f:
+    txt = f.readlines()
+print(txt)
+print('end of stage2')
